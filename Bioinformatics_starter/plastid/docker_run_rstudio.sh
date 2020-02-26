7a0c51bc5d9bb106ce7cd69bb8eceeb842ec2228
rimjhim roy choudhury <rimjhim.roy.ch@gmail.com>
2020-02-26 18:45:23 +0100

    Add Scenario

--- Bioinformatics_starter/plastid/docker_run_rstudio.sh
+++ Bioinformatics_starter/plastid/docker_run_rstudio.sh
@@ -1,2 +1,2 @@
 git clone https://github.com/rimjhimroy/plastid_sequence_analysis.git
-docker run --rm -p 8787:8787 -v $PWD/plastid_sequence_analysis:/home/rstudio/plastid_sequence_analysis -e ROOT=TRUE -e DISABLE_AUTH=true rocker/rstudio
+docker run --rm -p 8787:8787 -v $PWD/plastid_sequence_analysis:/home/rstudio/plastid_sequence_analysis -e ROOT=TRUE -e DISABLE_AUTH=true thibautjombart/adegenet_server
