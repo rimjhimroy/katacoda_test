d0ba0fdef842863e7990fc3a6108f89d27f43bd3
rimjhim roy choudhury <rimjhim.roy.ch@gmail.com>
2020-02-28 15:20:35 +0100

    add scenario

--- Bioinformatics_starter/repeat/docker_run_rstudio.sh
+++ Bioinformatics_starter/repeat/docker_run_rstudio.sh
@@ -2,7 +2,7 @@ git clone https://github.com/rimjhimroy/repeat_files.git
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh #Download miniconda installer
 chmod +x Miniconda3-latest-Linux-x86_64.sh #Make it executable
 ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda #install miniconda
-cat "export PATH=$PATH:$(pwd)" >> $HOME/.bashrc
+echo "export PATH=$PATH:$HOME/miniconda/bin" >> $HOME/.bashrc
 source $HOME/.bashrc
 conda config --set auto_activate_base False #set activation of base environment to false
 conda update conda #update conda
