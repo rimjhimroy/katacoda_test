631d8c50debf9e663ce11925b8103e6b4b335301
rimjhim roy choudhury <rimjhim.roy.ch@gmail.com>
2020-03-02 17:29:30 +0100

    Add Scenario

--- Bioinformatics_starter/repeat/docker_run_rstudio.sh
+++ Bioinformatics_starter/repeat/docker_run_rstudio.sh
@@ -6,5 +6,6 @@ echo "export PATH=$PATH:$HOME/miniconda/bin" >> $HOME/.bashrc
 source $HOME/.bashrc
 conda config --set auto_activate_base False #set activation of base environment to false
 #conda update conda -y #update conda
-conda install -c bioconda ltr_retriever -y
+conda create -n ltr_retriever -c bioconda ltr_retriever -y
+#conda install -c bioconda ltr_retriever -y
 docker run --rm -p 8787:8787 -v $PWD/repeat_files:/home/rstudio/repeat_files -e ROOT=TRUE -e DISABLE_AUTH=true thibautjombart/adegenet_server
