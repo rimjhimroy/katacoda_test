92da3d9fed6f533c5e03e9a133eace91b2ccab8e
rimjhim roy choudhury <rimjhim.roy.ch@gmail.com>
2020-03-02 19:23:27 +0100

    pull docker edta

--- Bioinformatics_starter/repeat/docker_run_rstudio.sh
+++ Bioinformatics_starter/repeat/docker_run_rstudio.sh
@@ -1,14 +1,3 @@
 git clone https://github.com/rimjhimroy/repeat_files.git
-wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh #Download miniconda installer
-chmod +x Miniconda3-latest-Linux-x86_64.sh #Make it executable
-./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda #install miniconda
-echo "export PATH=$PATH:$HOME/miniconda/bin" >> $HOME/.bashrc
-source $HOME/.bashrc
-conda config --set auto_activate_base False #set activation of base environment to false
-conda config --add channels conda-forge
-conda config --add channels bioconda
-#conda update conda -y #update conda
-conda create -n edta edta -y
-#conda install -c bioconda ltr_retriever -y
-conda init bash
+docker pull kapeel/edta
 docker run --rm -p 8787:8787 -v $PWD/repeat_files:/home/rstudio/repeat_files -e ROOT=TRUE -e DISABLE_AUTH=true thibautjombart/adegenet_server
